7d67552990eda9c221808ddb84d5e293d9c25aa1
Kent Hambrock <Kent.Hambrock@gmail.com>
2024-08-11 03:11:01 -0400

    Removed some incorrect config data

--- modules/sebrightbantam/home-assistant.nix
+++ modules/sebrightbantam/home-assistant.nix
@@ -8,24 +8,25 @@ let
 in
 {
   environment.systemPackages = [
-
+    python312Packages.isal
+    python312Packages.aiohttp-isal
   ];
 
   services.home-assistant = {
     enable = true;
     extraComponents = [
       # Components required to complete the onboarding
-      "esphome"
-      "met"
-      "radio_browser"
-      # Recommended for fast zlib compression
-      # https://www.home-assistant.io/integrations/isal
-      "isal"
+      # "esphome"
+      # "met"
+      # "radio_browser"
     ];
     config = {
       # Includes dependencies for a basic setup
       # https://www.home-assistant.io/integrations/default_config/
       default_config = {};
+      # Recommended for fast zlib compression
+      # https://www.home-assistant.io/integrations/isal
+      isal = {};
     };
   };
 
