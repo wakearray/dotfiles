03bbfd071331aa3a765ad7922799462f918d4b80
Kent Hambrock <Kent.Hambrock@gmail.com>
2024-08-29 01:12:48 -0400

    Added the start of a home manager config for user

--- home/kent/default.nix
+++ home/kent/default.nix
@@ -9,7 +9,13 @@
       # 
     ];
   };
-
+  
+  imports = [
+    ../common
+    ./starship.nix
+    ./zellij.nix
+  ];
+  
   # Editor Config helps enforce your preferences on editors
   editorconfig = {
     enable = true;
@@ -28,180 +34,97 @@
 
   programs = {
     home-manager.enable = true;
-    starship = {
+    # alacritty - A cross-platform, OpenGL terminal emulator
+    # https://github.com/alacritty/alacritty
+    alacritty = { 
       enable = true;
-      enableZshIntegration = true;
       settings = {
-        format = lib.concatStrings [
-          "[](color_01)"
-          "$os"
-          "$username"
-          "[](bg:color_02 fg:color_01)"
-          "$directory"
-          "[](fg:color_02 bg:color_03)"
-          "$git_branch"
-          "$git_status"
-          "[](fg:color_03 bg:color_04)"
-          "$c"
-          "$rust"
-          "$golang"
-          "$nodejs"
-          "$php"
-          "$java"
-          "$kotlin"
-          "$haskell"
-          "$python"
-          "[](fg:color_04 bg:color_05)"
-          "$docker_context"
-          "$conda"
-          "[](fg:color_05 bg:color_06)"
-          "$time"
-          "[ ](fg:color_06)"
-          "$line_break$character"
-        ];
-        palette = "gruvbox_dark";
-        palettes.gruvbox_dark = {
-          color_fg0 = "#184E77";
-          color_fg1 = "#D9ED92";
-          color_fg2 = "#83A598";
-          color_green = "#98971a";
-          color_purple = "#b16286";
-          color_red = "#cc241d";
-
-          color_01 = "#D9ED92";
-          color_02 = "#99D98C";
-          color_03 = "#52B69A";
-          color_04 = "#168AAD";
-          color_05 = "#1E6091";
-          color_06 = "#184E77";
-        };
-        os = {
-          disabled = false;
-          style = "bg:color_01 fg:color_fg0";
-          symbols = {
-            Windows = "󰍲";
-            Android = "";
-            Arch = "󰣇";
-            Debian = "󰣚";
-            NixOS = "";
-          };
-        };
-        username = {
-          show_always = true;
-          style_user = "bg:color_01 fg:color_fg0";
-          style_root = "bg:color_01 fg:color_fg0";
-          format = "[ $user ]($style)";
-        };
-        directory = {
-          style = "fg:color_fg0 bg:color_02";
-          format = "[ $path ]($style)";
-          truncation_length = 3;
-          truncation_symbol = "…/";
-
-          substitutions = {
-            "Documents" = "󰈙 ";
-            "Downloads" = " ";
-            "Music" = "󰝚 ";
-            "Pictures" = " ";
-            "Developer" = "󰲋 ";
-          };
-        };
-        git_branch = {
-          symbol = "";
-          style = "bg:color_03";
-          format = "[[ $symbol $branch ](fg:color_fg0 bg:color_03)]($style)";
-        };
-        git_status = {
-          style = "bg:color_03";
-          format = "[[($all_status$ahead_behind )](fg:color_fg0 bg:color_03)]($style)";
-        };
-        nodejs = {
-          symbol = "";
-          style = "bg:color_04";
-          format = "[[ $symbol( $version) ](fg:color_fg0 bg:color_04)]($style)";
-        };
-
-        c = {
-          symbol = " ";
-          style = "bg:color_04";
-          format = "[[ $symbol( $version) ](fg:color_fg0 bg:color_04)]($style)";
-        };
-
-        rust = {
-          symbol = "";
-          style = "bg:color_04";
-          format = "[[ $symbol( $version) ](fg:color_fg0 bg:color_04)]($style)";
-        };
-
-        golang = {
-          symbol = "";
-          style = "bg:color_04";
-          format = "[[ $symbol( $version) ](fg:color_fg0 bg:color_04)]($style)";
-        };
-
-        php = {
-          symbol = "";
-          style = "bg:color_04";
-          format = "[[ $symbol( $version) ](fg:color_fg0 bg:color_04)]($style)";
-        };
-
-        java = {
-          symbol = " ";
-          style = "bg:color_04";
-          format = "[[ $symbol( $version) ](fg:color_fg0 bg:color_04)]($style)";
-        };
-
-        kotlin = {
-          symbol = "";
-          style = "bg:color_04";
-          format = "[[ $symbol( $version) ](fg:color_fg0 bg:color_04)]($style)";
-        };
-
-        haskell = {
-          symbol = "";
-          style = "bg:color_04";
-          format = "[[ $symbol( $version) ](fg:color_fg0 bg:color_04)]($style)";
-        };
-
-        python = {
-          symbol = "";
-          style = "bg:color_04";
-          format = "[[ $symbol( $version) ](fg:color_fg0 bg:color_04)]($style)";
-        };
-
-        docker_context = {
-          symbol = "";
-          style = "bg:color_05";
-          format = "[[ $symbol( $context) ](fg:color_fg2 bg:color_05)]($style)";
-        };
-
-        conda = {
-          style = "bg:color_05";
-          format = "[[ $symbol( $environment) ](fg:color_fg2 bg:color_05)]($style)";
-        };
-
-        time = {
-          disabled = false;
-          use_12hr = true;
-          time_format = "%l:%M %P";
-          style = "bg:color_06";
-          format = "[[  $time ](fg:color_fg1 bg:color_06)]($style)";
-        };
-
-        line_break = {
-          disabled = false;
-        };
-
-        character = {
-          disabled = false;
-          success_symbol = "[](bold fg:color_green)";
-          error_symbol = "[](bold fg:color_red)";
-          vimcmd_symbol = "[](bold fg:color_green)";
-          vimcmd_replace_one_symbol = "[](bold fg:color_purple)";
-          vimcmd_replace_symbol = "[](bold fg:color_purple)";
-          vimcmd_visual_symbol = "[](bold fg:color_02)";
-        };
+        selection = {
+          save_to_clipboard = true;
+	};
+        cursor = {
+          style = 
+	  { 
+	    shape = "Underline";
+	    blinking = "Off";
+	  };
+	};
+        terminal = {
+          osc52 = "CopyPaste";
+	};
+        mouse = {
+          bindings = [{ 
+	    mouse = "Middle"; 
+	    action = "Paste";
+	  }];
+	};
       };
     };
+
+    # bat - a cat clone with wings
+    # https://github.com/sharkdp/bat
+    # More options found here:
+    # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.bat.enable
+    bat.enable = true;
+    
+    # btop - Resource monitor that shows usage and stats
+    # https://github.com/aristocratos/btop
+    # More options found here:
+    # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.btop.enable
+    btop.enable = true;   
+    
+    # fzf - The most supported command line fuzzy finder
+    # https://github.com/junegunn/fzf
+    # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.fzf.enable
+    fzf = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+
+    # git
+    # More options found here:
+    # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.git.enable
+    git = {
+      enable = true;
+      # delta - A syntax-highlighting pager for git, diff, grep, and blame output
+      # https://github.com/dandavison/delta
+      delta.enable = true; 
+    };
+    
+    # imv - a command line image viewer intended for use with tiling window managers
+    # https://sr.ht/~exec64/imv/
+    imv.enable = true;
+
+    # mpv - Command line video player
+    # https://github.com/mpv-player/mpv
+    # More options found here:
+    # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.mpv.enable
+    mpv.enable = true;
+
+    # rofi - A window switcher, application launcher and dmenu replacement
+    # https://github.com/davatorium/rofi
+    # More options found here:
+    # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.rofi.enable
+    rofi = {
+      enable = true;
+      terminal = "${pkgs.alacritty}/bin/alacritty";
+    };
+
+    # skim - `sk` - Fuzzy Finder in rust!
+    # https://github.com/lotabout/skim
+    # More options found here:
+    # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.skim.enable
+    skim = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+  };
+
+  services = {
+    # dunst - Lightweight and customizable notification daemon
+    # https://github.com/dunst-project/dunst
+    # More options found here:
+    # https://nix-community.github.io/home-manager/options.xhtml#opt-services.dunst.enable
+    dunst.enable = true;
   };
 }
