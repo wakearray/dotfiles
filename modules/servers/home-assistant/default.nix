660d5b2853305668844d0aba58bd7e9a6e123aee
Kent Hambrock <kent.hambrock@gmail.com>
2025-06-20 18:36:35 -0400

    Fixed tiny logic error in home-assistant logic

--- modules/servers/home-assistant/default.nix
+++ modules/servers/home-assistant/default.nix
@@ -5,7 +5,7 @@ in
 {
   # /modules/servers/home-assistant/
   imports = [
-    #./script-install.nix
+    ./script-install.nix
   ];
 
   options.servers.home-assistant = with lib; {
@@ -27,7 +27,7 @@ in
 
   config = lib.mkIf home-assistant.enable {
     warnings = (
-      lib.optionals (home-assistant.interface == null) "Interface cannot be null. Set this to the physical interface you want the home-assistant virtual machine to use."
+      lib.optionals (home-assistant.bridge.interface == null) "Interface cannot be null. Set this to the physical interface you want the home-assistant virtual machine to use."
     );
 
     virtualisation = {
