edd7cceeb6056046c610ad7feb3308561661f234
Kent Hambrock <kent.hambrock@gmail.com>
2025-08-10 15:45:29 -0400

    Added tuigreet console spam mitigations

--- modules/gui/greeters/tuigreet.nix
+++ modules/gui/greeters/tuigreet.nix
@@ -7,12 +7,26 @@ let
 in
 {
   options.gui.greeter.tuigreet = with lib; {
-    enable = mkEnableOption "Enable greetd with tui-greet to launch hyprland";
+    enable = mkEnableOption "Enable [greetd](https://git.sr.ht/~kennylevinsen/greetd) with [tuigreet](https://github.com/apognu/tuigreet) to launch hyprland.
+
+***Note:***
+  As `tuigreet` runs in the terminal after boot, occasionally slow systemd processes may write ontop of the UI. While there are no actual solutions to this, this module includes all the documented mitigations that I have found.";
   };
 
   config = lib.mkIf (gui.enable && tuigreet.enable) {
+    #
+    # greetd with tuigreet has serious issues with
+    # systemd console logging spam being writen on
+    # top of it. While there's no true solution,
+    # this module contains most documented mitigations.
+    #
+    # Sadly, none of these mitigations work 100%
+    # of the time for 100% of users.
+    #
     services.greetd = {
       enable = true;
+      # Run greetd on tty2 to avoid systemd logging spam
+      vt = 2;
       settings = {
         default_session = {
           command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd \"${hyprlandStart}\"";
@@ -22,5 +36,22 @@ in
     };
 
     environment.systemPackages = [ pkgs.tuigreet ];
+
+    # Ensure all systemd logging happens on tty1.
+    # This allows running tuigreet on a different
+    # vt to avoid console spam.
+    boot.kernelParams = [ "console=tty1" ];
+
+    # Wait till multi-user target is triggered
+    # to avoid the majority of the systemd logging spam
+    systemd.services.greetd = {
+      unitConfig = {
+        After = lib.mkOverride 0 [ "multi-user.target" ];
+      };
+      serviceConfig = {
+        # Use `idle` rather than `simple` to help avoid systemd logging spam
+        Type = "idle";
+      };
+    };
   };
 }
