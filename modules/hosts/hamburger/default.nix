af0836b226cc10f8c9a0633cd24dd131af4399f3
Kent Hambrock <kent.hambrock@gmail.com>
2025-08-13 14:44:24 -0400

    Updatign SOPS keys

--- modules/hosts/hamburger/default.nix
+++ modules/hosts/hamburger/default.nix
@@ -1,6 +1,5 @@
 { domain, ... }:
 {
-
   config = {
     servers = {
       nginx = {
@@ -10,6 +9,20 @@
       mail = {
         enable = true;
         domain = domain;
+        secrets = let
+          opts = {
+            sopsFile = ./mailserver.yaml;
+            mode     = "0400";
+            owner    = "virtualMail";
+            group    = "virtualMail";
+          };
+        in
+        {
+          mail-server-admin = opts;
+          mail-server-noreply = opts;
+          mail-server-kent = opts;
+          mail-server-jess = opts;
+        };
       };
     };
   };
