2d00ddd4cb2a72ee48f764aaf1b37f47f5712624
Kent Hambrock <kent.hambrock@gmail.com>
2025-11-12 03:38:43 -0500

    Additional missing arguments

--- hosts/moonfish/hardware-configuration.nix
+++ hosts/moonfish/hardware-configuration.nix
@@ -30,9 +30,9 @@
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = true;
+  networking.useDHCP = lib.mkDefault true;
   # networking.interfaces.enp3s0.useDHCP = lib.mkDefault true;
 
-  nixpkgs.hostPlatform = "x86_64-linux";
-  hardware.cpu.amd.updateMicrocode = true;
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault true;
 }
