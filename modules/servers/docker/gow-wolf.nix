98f63b5130c796c3db7228aeb3fcff5e3b8b9247
Kent Hambrock <kent.hambrock@gmail.com>
2025-11-16 04:22:01 -0500

    Removed Nvidia from wolf module

--- modules/servers/docker/gow-wolf.nix
+++ modules/servers/docker/gow-wolf.nix
@@ -13,8 +13,10 @@ in
         image = "ghcr.io/games-on-whales/wolf:stable";
         environment = {
           # Used for manual method
-          NVIDIA_DRIVER_VOLUME_NAME = "nvidia-driver-vol";
-          WOLF_RENDER_NODE = "/dev/dri/renderD129";
+          #NVIDIA_DRIVER_VOLUME_NAME = "nvidia-driver-vol";
+
+          # Force use of a specific GPU (D128 for AMD and D129 for Nvidia)
+          WOLF_RENDER_NODE = "/dev/dri/renderD128";
 
           # Used for nvidia-container-toolkit method
           # NVIDIA_DRIVER_CAPABILITIES = "all";
@@ -27,7 +29,7 @@ in
           "/run/udev:/run/udev:rw"
 
           # Used for manual method
-          "nvidia-driver-vol:/usr/nvidia:rw"
+          # "nvidia-driver-vol:/usr/nvidia:rw"
         ];
         devices = [
           "/dev/dri"
@@ -35,13 +37,13 @@ in
           "/dev/uhid"
 
           # Used for manual method
-          "/dev/nvidia-uvm"
-          "/dev/nvidia-uvm-tools"
-          "/dev/nvidia-caps/nvidia-cap1"
-          "/dev/nvidia-caps/nvidia-cap2"
-          "/dev/nvidiactl"
-          "/dev/nvidia0"
-          "/dev/nvidia-modeset"
+          # "/dev/nvidia-uvm"
+          # "/dev/nvidia-uvm-tools"
+          # "/dev/nvidia-caps/nvidia-cap1"
+          # "/dev/nvidia-caps/nvidia-cap2"
+          # "/dev/nvidiactl"
+          # "/dev/nvidia0"
+          # "/dev/nvidia-modeset"
         ];
         ports = [
           # HTTPS
@@ -68,11 +70,41 @@ in
       };
     };
 
+    hardware.uinput.enable = true;
+
+    services.udev.extraRules = /*udev*/ ''
+      # Allows Wolf to acces /dev/uinput
+      KERNEL=="uinput", SUBSYSTEM=="misc", MODE="0660", GROUP="input", OPTIONS+="static_node=uinput"
+
+      # Allows Wolf to access /dev/uhid
+      KERNEL=="uhid", TAG+="uaccess"
+
+      # Move virtual keyboard and mouse into a different seat
+      SUBSYSTEMS=="input", ATTRS{id/vendor}=="ab00", MODE="0660", GROUP="input", ENV{ID_SEAT}="seat9"
+
+      # Joypads
+      SUBSYSTEMS=="input", ATTRS{name}=="Wolf X-Box One (virtual) pad", MODE="0660", GROUP="input"
+      SUBSYSTEMS=="input", ATTRS{name}=="Wolf PS5 (virtual) pad", MODE="0660", GROUP="input"
+      SUBSYSTEMS=="input", ATTRS{name}=="Wolf gamepad (virtual) motion sensors", MODE="0660", GROUP="input"
+      SUBSYSTEMS=="input", ATTRS{name}=="Wolf Nintendo (virtual) pad", MODE="0660", GROUP="input"
+    '';
+
     # Enabled for nvidia-container-toolkit method
     # hardware.nvidia-container-toolkit = {
     #   enable = true;
     #   package = pkgs.nvidiaCtkPackages.nvidia-container-toolkit-docker;
     # };
+
+    systemd.services.nvidia-container-cli-info = {
+      description = "Run nvidia-container-cli --load-kmods info on boot";
+      wantedBy = [ "multi-user.target" ];
+      after = [ "nvidia-persistenced.service" ]; # Ensure NVIDIA services are up
+      serviceConfig = {
+        Type = "oneshot";
+        ExecStart = "${pkgs.nvidia-container-toolkit}/bin/nvidia-container-cli --load-kmods info";
+        User = "root"; # Run as root for necessary permissions
+      };
+    };
 
     environment.systemPackages = with pkgs; [
       libnvidia-container
